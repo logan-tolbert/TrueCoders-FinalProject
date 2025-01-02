245b0866505bdf4961b4f1550c2ffb1c59baa559
logan-Tolbert <lodev205@gmail.com>
2025-01-02 10:43:43 -0600

    Add DataGateway project and refactor data access layer
    - Rename 'SqlDataAccess' to 'DataGateway'
    - Add Nuget packages Dapper (v2.1.35), Microsoft.Data.SqlClient (v5.2.2), Microsoft.Extensions.Configuration.Abstractions (v9.0.0)
    - Create `DataAccess.cs` to implement the `IDataAccess` interface for data operations using Dapper and SQL Server.
    - Define the `IDataAccess` interface in `IDataAccess.cs`.

---
+++ SqlDataAccess/DataAccess.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using DataGateway;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+
+namespace App.Library.Context
+{
+    public class SqlDataAccess : IDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName, bool isStoredProcedure = false)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName)!;
+
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                return connection.Query<T>(sqlStatement, parameters, commandType: commandType).ToList();
+            }
+        }
+
+        public void SaveData<U>(string sqlStatement, U parameters, string connectionStringName, bool isStoredProcedure = false)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName)!;
+
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameters, commandType: commandType);
+            }
+        }
+    }
+}
---
+++ SqlDataAccess/IDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGateway
+{
+    public interface IDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName, bool isStoredProcedure = false);
+        void SaveData<U>(string sqlStatement, U parameters, string connectionStringName, bool isStoredProcedure = false);
+    }
+}
